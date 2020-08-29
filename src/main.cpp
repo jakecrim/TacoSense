62b8484cd1c0937ab844a46f67bc8e549728df5b
Jacob Higgins <higginsjake909@gmail.com>
2020-08-29 01:48:52 -0500

    Changed to sending a struct instead of a simple string

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,36 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+// Variables
+typedef struct transcieve_message {
+	int distance;
+} transcieve_message;
+
+
+
 //Declarations
 void wireless_Open(void);
 esp_now_recv_cb_t receive_message(void);
 esp_now_send_cb_t sent_message(void);
 void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen);
 void transmit(const String &data);
+void transmit2(transcieve_message data);
+
 
 int main(void)
 {
 	Serial.println(" TEST 1:)");
+
+	transcieve_message message1;
+
 	wireless_Open();
 	for(;;)
 	{
-		delay(1000);
-		transmit("howdy partner");
+		delay(2500);
+		// transmit("howdy partner");
+		message1.distance = 15;
+		transmit2(message1);
+		// transmit2((uint8_t *) &message1);
 	}
 }
 
@@ -40,6 +55,48 @@ void wireless_Open()
 		ESP.restart();
 	}
 	
+}
+
+void transmit2(transcieve_message data)
+{
+	// Transmits to any device in range
+	uint8_t transmitAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+	esp_now_peer_info_t peer = {};
+	memcpy(&peer.peer_addr, transmitAddr, 6);
+	if (!esp_now_is_peer_exist(transmitAddr))
+	{
+		esp_now_add_peer(&peer);
+	}
+	esp_err_t result = esp_now_send(transmitAddr, (uint8_t *) &data , sizeof(data));
+
+	if (result == ESP_OK)
+	{
+		Serial.println("Broadcast message success");
+	}
+	else if (result == ESP_ERR_ESPNOW_NOT_INIT)
+	{
+		Serial.println("ESPNOW not Init.");
+	}
+	else if (result == ESP_ERR_ESPNOW_ARG)
+	{
+		Serial.println("Invalid Argument");
+	}
+	else if (result == ESP_ERR_ESPNOW_INTERNAL)
+	{
+		Serial.println("Internal Error");
+	}
+	else if (result == ESP_ERR_ESPNOW_NO_MEM)
+	{
+		Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+	}
+	else if (result == ESP_ERR_ESPNOW_NOT_FOUND)
+	{
+		Serial.println("Peer not found.");
+	}
+	else
+	{
+		Serial.println("Unknown error");
+	}
 }
 
 void transmit(const String &data)
@@ -95,27 +152,23 @@ void transmit(const String &data)
 
 void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
 {
-	// only allow a maximum of 250 characters in the message + a null terminating byte
-	char buffer[ESP_NOW_MAX_DATA_LEN + 1];
-	int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
-	strncpy(buffer, (const char *)data, msgLen);
-	// make sure we are null terminated
-	buffer[msgLen] = 0;
-	// format the mac address
-	char macStr[18];
-	// debug log the message to the serial port
-	Serial.printf("Received message from: %s - %s\n", macStr, buffer);
-	// what are our instructions
-	// printf("%s \n", buffer);
-	// if (strcmp("on", buffer) == 0)
-	// {
-	// ledOn = true;
-	// }
-	// else
-	// {
-	// ledOn = false;
-	// }
-	// digitalWrite(2, ledOn);
+	transcieve_message recieved;
+	memcpy(&recieved, data, sizeof(data));
+	printf("Bytes Recieved: %d \n", dataLen);
+
+	printf("Distance: %d \n", recieved.distance);
+
+	// // only allow a maximum of 250 characters in the message + a null terminating byte
+	// char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+	// int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+	// printf("data -->%c ", *data);
+	// strncpy(buffer, (const char *)data, msgLen);
+	// // make sure we are null terminated
+	// buffer[msgLen] = 0;
+	// // format the mac address
+	// char macStr[18];
+	// // debug log the message to the serial port
+	// Serial.printf("Received message from: %s - %c\n", macStr, *buffer);
 }
 
 // esp_now_send_cb_t sent_message()
